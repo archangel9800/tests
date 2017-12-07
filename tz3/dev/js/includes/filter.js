df5f8355931b8854e28c8ad6649811c95cd5e868
archangel9800 <pain_s@mail.ru>
2017-12-07 22:04:21 +0200

    done tz3

--- tz3/dev/js/includes/filter.js
+++ tz3/dev/js/includes/filter.js
@@ -1,6 +1,3 @@
-$(document).ready(function () {
-	svg4everybody({});
-});
     $(document).ready(function(){
         $("#filter .buttons").on("click", function (event) {
             event.preventDefault();
@@ -10,12 +7,4 @@ $(document).ready(function () {
             $('#filter .imgBl .imgCont').removeClass('visible');
             $('#filter .imgBl [class~="'+claSs+'"]').addClass('visible');
         });     
-    });
-     $(document).ready(function(){
-        $("#header .buttons").on("click", function (event) {
-            event.preventDefault();
-            var id  = $(this).attr('data-go'),
-                top = $(id).offset().top;
-            $('body,html').animate({scrollTop: top}, 1000);
-        });
     });
